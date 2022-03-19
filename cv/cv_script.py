a4c3e6fd60a784735e9195ebf796a28a5bc319ea
bueinat <62245924+bueinat@users.noreply.github.com>
2022-03-19 20:44:36 +0200

    update database structure + try working out on features

--- cv/cv_script.py
+++ cv/cv_script.py
@@ -1,9 +1,9 @@
 # %% [markdown]
 # # Computer Vision
-# 
-# 16/11/2021  
+#
+# 16/11/2021
 # read images from video and analyse them.
-# 
+#
 
 # %%
 # %pylab inline
@@ -24,13 +24,14 @@ VIDEO_PATH = 'videos/examples/Odor4.avi'
 
 # %% [markdown]
 # ## convert video to frames
-# 
+#
 
 # %% [markdown]
 # ## initial code
-# 
-# mainly taken from [here](https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/).   
+#
+# mainly taken from [here](https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/).
 # this code roughly finds the path in which the rat went. It goes over the frames and tries to detect from the edges where the rat is
+
 
 def video_to_frames(video_path, frames_path, video_name=None, include_video_name=False):
     # create filename if doesn't exist and capture whole video
@@ -39,13 +40,13 @@ def video_to_frames(video_path, frames_path, video_name=None, include_video_name
     vidcap = cv2.VideoCapture(video_path)
     os.makedirs(frames_path, exist_ok=True)
     count = 0
-    
+
     # make sure the video wasn't extracted already
     success, image = vidcap.read()
     frame_name = f"{frames_path}\\{video_name}_frame{count}.jpg" if include_video_name else f"{frames_path}\\frame{count}.jpg"
     if os.path.exists(frame_name):
         raise Exception('the video was already extracted')
-    
+
     # write all frames to images
     while success:
         frame_name = f"{frames_path}\\{video_name}_frame{count}.jpg" if include_video_name else f"{frames_path}\\frame{count}.jpg"
@@ -55,6 +56,8 @@ def video_to_frames(video_path, frames_path, video_name=None, include_video_name
         success, image = vidcap.read()
         count += 1
 # %%
+
+
 def get_arena_limits(gray):
     thresh = cv2.threshold(
         gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
@@ -80,11 +83,14 @@ def find_contours(gray, alims, firstFrame):
 
     # dilate the thresholded image to fill in holes, then find contours on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(
+        thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     return cnts
 
 # %%
+
+
 def rat_path(video_path):
     vs = cv2.VideoCapture(video_path)
     firstFrame = None
@@ -101,17 +107,18 @@ def rat_path(video_path):
         # if the frame could not be grabbed, then we have reached the end of the video
         if frame is None:
             break
-        
+
         # resize the frame, convert it to grayscale, and blur it
         frame = imutils.resize(frame, width=500)
         frames.append(frame.copy())
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        
+
         # if the first frame is None, initialize it
         if firstFrame is None:
             alims = get_arena_limits(gray)
-            firstFrame = gray[alims['ymin']:alims['ymax'], alims['xmin']:alims['xmax']]
+            firstFrame = gray[alims['ymin']:alims['ymax'],
+                              alims['xmin']:alims['xmax']]
             continue
 
         cnts = find_contours(gray, alims, firstFrame)
@@ -122,13 +129,15 @@ def rat_path(video_path):
                 continue
             # compute the bounding box for the contour
             (x, y, w, h) = cv2.boundingRect(c)
-            rat_rects[frame_num] = {'x': x, 'y': y, 'w': w, 'h': h, 'm':(w + h) / 2}
+            rat_rects[frame_num] = {'x': x, 'y': y,
+                                    'w': w, 'h': h, 'm': (w + h) / 2}
 
     # cleanup the camera and close any open windows
     vs.release()
     cv2.destroyAllWindows()
 
     return frames, rat_rects, alims
+
 
 # %%
 os.chdir('cv')
@@ -139,7 +148,8 @@ data_path = f"{archive_path}\\{video_name}"
 try:
     os.mkdir(data_path)
 except FileExistsError:
-    raise FileExistsError(f'a video named {video_name} already exists in archive. You can use it or give the new video a different name')
+    raise FileExistsError(
+        f'a video named {video_name} already exists in archive. You can use it or give the new video a different name')
 shutil.copy2(VIDEO_PATH, data_path)
 frames_path = f".\\archive\\{video_name}\\frames"
 video_to_frames(VIDEO_PATH, frames_path)
@@ -155,7 +165,8 @@ path = raw_data.set_index('x').y
 # %%
 raw_data['vx'] = raw_data.x.diff() / raw_data.time.diff()
 raw_data['vy'] = raw_data.y.diff() / raw_data.time.diff()
-raw_data['r_tot'] = np.sqrt((raw_data.x - raw_data.x.iloc[0]) ** 2 + (raw_data.y - raw_data.y.iloc[0]) ** 2)
+raw_data['r_tot'] = np.sqrt(
+    (raw_data.x - raw_data.x.iloc[0]) ** 2 + (raw_data.y - raw_data.y.iloc[0]) ** 2)
 raw_data['r'] = np.sqrt(raw_data.x.diff() ** 2 + raw_data.y.diff() ** 2)
 raw_data['v'] = np.sqrt(raw_data.vx ** 2 + raw_data.vy ** 2)
 
@@ -165,11 +176,16 @@ raw_data['a'] = np.sqrt(raw_data.ax ** 2 + raw_data.ay ** 2)
 
 # %%
 dists = np.sqrt(raw_data.x.diff() ** 2 + raw_data.y.diff() ** 2)
+
+
 def aireal_dist(dfx, dfy):
     return np.sqrt((dfx.iloc[0] - dfx.iloc[-1]) ** 2 + (dfy.iloc[0] - dfy.iloc[-1]) ** 2)
+
+
 win_size = 100
 
-raw_data["adist"] = rolling_apply(aireal_dist, win_size, raw_data.x, raw_data.y)
+raw_data["adist"] = rolling_apply(
+    aireal_dist, win_size, raw_data.x, raw_data.y)
 raw_data["rdist"] = raw_data.r.rolling(win_size).sum()
 
 
@@ -177,43 +193,60 @@ raw_data["rdist"] = raw_data.r.rolling(win_size).sum()
 # ### Upload data to server
 
 # %%
-cluster = "mongodb+srv://john:1234@cluster0.9txls.mongodb.net/real_test?retryWrites=true&w=majority"
+cluster = "mongodb+srv://john:1234@cluster0.9txls.mongodb.net/real_test?retryWrites=true&w=majority"     
 mnge.register_connection(alias='core', host=cluster)
 
 
 # %%
-class Analysis(mnge.EmbeddedDocument):    
-    timestep = mnge.IntField(required=True)
+# those are just for introducing purposes
+# running the full classes ends up with errors because they don't know each other
+class Analysis(mnge.Document):
+    timestep = mnge.ListField(mnge.IntField())
     
-    x = mnge.FloatField(required=True)
-    y = mnge.FloatField(required=True)
-    vx = mnge.FloatField(required=True)
-    vy = mnge.FloatField(required=True)
-    ax = mnge.FloatField(required=True)
-    ay = mnge.FloatField(required=True)
-    curviness = mnge.FloatField(required=True)
+class Video(mnge.Document):
+    registered_date = mnge.DateTimeField(default=datetime.datetime.now)
     
-    path = mnge.StringField(required=True)
-    is_grooming = mnge.BooleanField(default=False)
-    is_rearing = mnge.BooleanField(default=False)
-    is_running = mnge.BooleanField(default=False)
-    is_freezing = mnge.BooleanField(default=False)
-    is_nose_casting = mnge.BooleanField(default=False)
-    video_id = mnge.ObjectIdField(required=True)
+#%%
+class Analysis(mnge.Document):
+    timestep = mnge.ListField(mnge.IntField(), required=True)
+    
+    x = mnge.ListField(mnge.FloatField(), required=True)
+    y = mnge.ListField(mnge.FloatField(), required=True)
+    vx = mnge.ListField(mnge.FloatField(), required=True)
+    vy = mnge.ListField(mnge.FloatField(), required=True)
+    ax = mnge.ListField(mnge.FloatField(), required=True)
+    ay = mnge.ListField(mnge.FloatField(), required=True)
+    curviness = mnge.ListField(mnge.FloatField(), required=True)
+    
+    path = mnge.ListField(mnge.StringField(required=True))
+    is_grooming = mnge.ListField(mnge.BooleanField(default=False))
+    is_rearing = mnge.ListField(mnge.BooleanField(default=False))
+    is_running = mnge.ListField(mnge.BooleanField(default=False))
+    is_freezing = mnge.ListField(mnge.BooleanField(default=False))
+    is_nose_casting = mnge.ListField(mnge.BooleanField(default=False))
+    video = mnge.ReferenceField(Video)
+    
+    meta = {
+        'db_alias': 'core',
+        'collection': 'analysis'
+    }
 
 # %%
+
+
 class Video(mnge.Document):
     registered_date = mnge.DateTimeField(default=datetime.datetime.now)
     name = mnge.StringField(required=True)
-    length = mnge.IntField(required=True) # should be in seconds
+    length = mnge.IntField(required=True)
     description = mnge.StringField(required=True)
     link_to_data = mnge.StringField(required=True)
-    analysis = mnge.EmbeddedDocumentListField(Analysis)
-
+    analysis = mnge.ReferenceField(Analysis)
+    
     meta = {
         'db_alias': 'core',
         'collection': 'videos'
     }
+
 
 # %%
 video = Video()
@@ -235,24 +268,21 @@ uploadabale_data.loc[:, 'is_running'] = False
 uploadabale_data.loc[:, 'is_freezing'] = False
 uploadabale_data.loc[:, 'is_nose_casting'] = False
 
-uploadabale_data.loc[:, 'video_id'] = video.id
- 
 
 # %%
-for idx, row in uploadabale_data.iterrows():
-    video = Video.objects(id=video.id).first()
+video = Video.objects(id=video.id).first()
+ana = Analysis()
+ana.timestep = list(uploadabale_data.index)
 
-    ana = Analysis()
-    ana.timestep = idx
-    for c in uploadabale_data.columns:
-        ana[c] = row[c]
-        if c.startswith('is_'):
-            ana[c] = bool(row[c])
-
-    video.analysis.append(ana)
-    try:
-        video.save()
-        # print(f"success! {idx}")
-    except Exception as e:
-        print(f"something went wrong {idx}")
-        print(e.message)
+for c in uploadabale_data.columns:
+    exec(f"ana.{c} = list(uploadabale_data['{c}'])")
+    
+ana.video = video.id
+video.analysis = ana
+ana.save()
+video.update(analysis=ana.id)
+try:
+    video.save()
+except Exception as e:
+    print(f"something went wrong")
+    print(e.message)
